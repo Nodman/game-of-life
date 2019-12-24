ac5d2a804ef5a536fdb4605894f71605be0aab96
Nodman <nodman@icloud.com>
2019-12-24 22:15:28 +0200

    refactor a bit

--- src/components/root/index.tsx
+++ src/components/root/index.tsx
@@ -1,9 +1,10 @@
-import React, {useEffect, useState, useReducer} from 'react';
+import React, {useEffect , useReducer} from 'react';
 import Grid from '../grid';
 import {createGrid, calculateNextGeneration, spawnRandomCells} from '../../utils';
 
 const GRID_SIZE = 50;
 const CELL_TO_SPAWN = 300;
+const GRID = createGrid(GRID_SIZE);
 const INITIAL_STATE = spawnRandomCells(GRID_SIZE, CELL_TO_SPAWN);
 
 const reducer = (state: CellsSet, action: {type: string}) => {
@@ -16,19 +17,15 @@ const reducer = (state: CellsSet, action: {type: string}) => {
 };
 
 const Root = () => {
-  const [grid, setGrid] = useState<Grid | null>(null);
   const [currentCycleState, dispatch] = useReducer(reducer, INITIAL_STATE);
 
   useEffect((): void => {
-    const newGrid = createGrid(GRID_SIZE);
-    setGrid(newGrid);
-
     setInterval(() => {
       dispatch({type: 'TICK'});
     }, 100);
   }, []);
 
-  return grid ? <Grid grid={grid} currentCycle={currentCycleState} /> : null;
+  return <Grid grid={GRID} currentCycle={currentCycleState} />;
 };
 
 export default Root;
